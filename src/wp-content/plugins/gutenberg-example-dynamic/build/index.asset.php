385cfad395dc5989e2bd3f2ee2689d453e5253ef
Adam Wood <adam.wood@automattic.com>
2022-08-18 16:12:48 +1200

    Build for prod

--- src/wp-content/plugins/gutenberg-example-dynamic/build/index.asset.php
+++ src/wp-content/plugins/gutenberg-example-dynamic/build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-data', 'wp-element'), 'version' => 'd3b919544c2047e32819');
+<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-data', 'wp-element'), 'version' => '9f8c1ffe2f549b082cef');
